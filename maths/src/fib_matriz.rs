4586def24a2125438c18482edb3a366a6b7db875
Yui <dev@sleepyyui.com>
2024-01-23 08:36:08 +0100

    add types

--- maths/src/fib_matriz.rs
+++ maths/src/fib_matriz.rs
@@ -2,12 +2,12 @@ use num::{BigUint, One, Zero};
 use rayon::prelude::*;
 
 pub(crate) fn fib_matrix(n: usize) -> BigUint {
-    let mut f = vec![vec![BigUint::one(), BigUint::one()],
+    let mut f: Vec<Vec<BigUint>> = vec![vec![BigUint::one(), BigUint::one()],
                      vec![BigUint::one(), BigUint::zero()]];
-    let mut res = vec![vec![BigUint::one(), BigUint::zero()],
+    let mut res: Vec<Vec<BigUint>> = vec![vec![BigUint::one(), BigUint::zero()],
                        vec![BigUint::zero(), BigUint::zero()]];
 
-    let mut n = n;
+    let mut n: usize = n;
     while n > 0 {
         println!("{} iterations remaining.", n);
         if n & 1 == 1 {
@@ -19,15 +19,15 @@ pub(crate) fn fib_matrix(n: usize) -> BigUint {
     res[0][1].clone()
 }
 
-use std::sync::Mutex;
+use std::sync::{Mutex, MutexGuard};
 
 fn mat_mul(a: &Vec<Vec<BigUint>>, b: &Vec<Vec<BigUint>>) -> Vec<Vec<BigUint>> {
-    let res = Mutex::new(vec![vec![BigUint::zero(); 2]; 2]);
+    let res: Mutex<Vec<Vec<BigUint>>> = Mutex::new(vec![vec![BigUint::zero(); 2]; 2]);
     (0..2).into_par_iter().for_each(|i| {
         for j in 0..2 {
             for k in 0..2 {
-                let mut res = res.lock().unwrap();
-                let mut res_ij = res[i][j].clone();
+                let mut res: MutexGuard<Vec<Vec<BigUint>>> = res.lock().unwrap();
+                let mut res_ij: BigUint = res[i][j].clone();
                 res_ij += &a[i][k] * &b[k][j];
                 res[i][j] = res_ij;
             }
--- maths/src/three_x_plus_one.rs
+++ maths/src/three_x_plus_one.rs
@@ -4,8 +4,8 @@ extern crate num;
 // 3x+1 function
 // return the count and the highest number
 pub(crate) fn three_x_plus_one (mut n: u128) -> (u128, u128) {
-    let mut count = 0;
-    let mut highest = 0;
+    let mut count: u128 = 0;
+    let mut highest: u128 = 0;
     while n != 1 {
         //println!("{}", n);
         if n % 2 == 0 {
